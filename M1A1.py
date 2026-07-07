f4339fe035852b4339474672e191ebbd9b48079c
sheemamn <125912869+sheemamn@users.noreply.github.com>
2026-07-07 11:29:54 +0530

    Print Command

---
+++ M1A1.py
@@ -0,0 +1,12 @@
+# Printing a greeting message
+print("Hello, world!")
+
+# Printing my name
+print("My name is Sheema.")
+
+# Printing a fun fact about myself
+# print("I love playing soccer.")
+print("I love reading books.")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)

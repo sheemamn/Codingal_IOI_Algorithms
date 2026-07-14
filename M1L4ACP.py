e00221f849fd41ad15b202d0ff036aa094f0eda7
sheemamn <125912869+sheemamn@users.noreply.github.com>
2026-07-14 10:58:05 +0530

    armstrong or not

---
+++ M1L4ACP.py
@@ -0,0 +1,17 @@
+# Armstrong Number Check
+
+num = int(input("Enter a number: "))
+
+order = len(str(num))
+temp = num
+sum_of_powers = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** order
+    temp //= 10
+
+if num == sum_of_powers:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
